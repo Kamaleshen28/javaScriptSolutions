9ef4d03dc2f0bb695268281c450bc91d4cda4be4
Kamaleshen S <kamaleshen.s@gmail.com>
2023-01-16 14:59:21 +0530

    fix: use eslint to format

--- countVowels.js
+++ countVowels.js
@@ -1,24 +1,24 @@
 function countVowelsRegular(str){
     let count=0;
-    const vowels = "aeiou";
-    str.split("").forEach(fun);
+    const vowels = 'aeiou';
+    str.split('').forEach(fun);
 
     function fun(item){
         count = count + ((vowels.indexOf(item) !=-1 )? 1: 0); 
     }
     return count;
 }
-console.log(countVowelsRegular("codeacademy"));
+console.log(countVowelsRegular('codeacademy'));
 
 const countVowelsArrow = (str) => {
     let count=0;
-    const vowels = "aeiou";
+    const vowels = 'aeiou';
 
     const fun = (item) => count = (vowels.indexOf(item) !=-1 )? count+1: count; 
     
-    str.split("").forEach(fun);
+    str.split('').forEach(fun);
     return count;
-}
-console.log(countVowelsArrow("kamal"));
+};
+console.log(countVowelsArrow('kamal'));
 
 module.exports = {countVowelsArrow, countVowelsRegular};
